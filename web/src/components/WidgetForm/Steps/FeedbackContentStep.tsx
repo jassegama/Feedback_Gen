6e4dcd4d8493fe9df49737a1a76d7afbe628bbe5
Jasse Gama <jassegama@gmail.com>
2022-05-12 14:51:25 +1200

    Back End Coded

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { FeedbackType, feedbackTypes } from "../Index";
 import { ScreenshotButton } from "./ScreenshotButton";
 
@@ -17,17 +19,22 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null>(null)
     const [comment, setComment] = useState('')
+    const [ isSendingFeedback, setIsSendingFeedback ] = useState(false)
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-    function handleSubmitFeedback(event: FormEvent) {
-        event.preventDefault()
-        console.log(
-            {
-                screenshot,
-                comment
-            }
-        )
+    async function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+
+        setIsSendingFeedback(true)
+
+        await api.post('/feedbacks', {
+            type: feedbackType,
+            comment,
+            screenshot
+        })
+
+        setIsSendingFeedback(false)
         onFeedbackSent();
     }
 
@@ -39,7 +46,7 @@ export function FeedbackContentStep({
                 className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
                 onClick={onFeedbackRestartRequested}
                 >
-                    <ArrowLeft weight="bold" clasName="w-4 h-4"  />
+                    <ArrowLeft weight="bold" className="w-4 h-4"  />
                 </button>
                 
                 <span className="text-xl leading-6 flex items-center gap-2">
@@ -66,10 +73,10 @@ export function FeedbackContentStep({
 
                     <button 
                     type="submit"
-                    disabled={comment.length === 0}
+                    disabled={comment.length === 0 || isSendingFeedback}
                     className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
-                    Submit feedback        
+                    { isSendingFeedback ? <Loading /> : 'Submit feedback' }
                     </button>
                 </footer>
             </form>
